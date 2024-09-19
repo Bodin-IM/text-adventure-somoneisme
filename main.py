e341f5a8e50cbe0ed98c9d03a0f28eccf1195d86
somoneisme <natanielellefsen@gmail.com>
2024-09-19 14:38:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ distance = 0
 landmarkPosition = 0
 answer = 0
 climbingTrue = 0
+endingTryAgain = False
     
     
 def bleeding():
@@ -83,6 +84,8 @@ def roomStart(position,RoomNumber):
         print("do you want to go back to the last room type yes or no")
         answer = input(": ")
         if answer == "yes" and RoomNumber == 1:
+            print("You cant go through this yet")
+        if answer == "yes" and RoomNumber == 2:
             room(1,20,20,20,False,None,None,True,10,True,True,0,)
 def roomExit(position):
     global playerPosition, answer
@@ -152,14 +155,18 @@ def dayPass():
         bleed -= 1
         print("you are bleeding")
     heat -= outsideHeat
-def ending(ending):
-    if ending == 1:
+def ending(Ending):
+    global endingTryAgain
+    if Ending == 1:
         print(f"you were found dead by the other climbers")
         print("and became a landmark for other people climbing the mountain")
     print(f"you had these stats at the end of you run")
     print(f"{health}health, {stamina}, stamina {food} food, {water} water, {heat} heat, {oxygen} oxygen")
     print("THE END")
+    print("do you want to try again type yes or no")
+    endingTryAgain = input(": ").lower()
     
+
 
 
     
@@ -204,3 +211,43 @@ if answer == "b":
     elif answer == "b":
         print("you walk for a whole day and pass out")
         ending(1)
+while endingTryAgain == "yes":
+    print("you are lost on a snowy mountain after falling off a cliff")
+    print("its cold dark and your wet from the snow")
+    print("the other climbers think you are dead so theres no one coming to help you")
+    print("from what you see in the surrounding area you have a two choises")
+    print("a try to climb back up the way you came")
+    print("b go up another way")
+    answer = input().lower()
+    if answer == "a":
+        print("you now stand before a large cliff the same one you fell down moments ago you have decided to try to climb back up")
+        print("you attemt to climb up you have no gear so this wont be easy but it might be possible")
+        climbing(3)
+        
+    if answer == "b":
+        print("you find yourself climbing a new part of the mountain wich you have never climed before")
+        print("after climbing a while you find a cave")
+        print("yet again you find yourself with two options")
+        print("a stay a night in the cave")
+        print("b keep going up")
+        answer = input("")
+        if answer == "a":   
+            print("you decided to stay in the cave for the night")
+            print("you can now choose if you want to")
+            print("a try too set up a campfire for warmth")
+            print("b try too get some rest")
+            answer = input("")
+            if answer == "a":
+                print("you found some wood and set up a campfire")
+                print("you gained 20 warmth")
+                heat += 20
+                dayPass()
+                room(1, 20,20,10,False,None,None,True,10,)
+            if answer == "b":
+                print("you slept for the night and gained 20 stamina")
+                stamina += 20
+                dayPass()
+                room(1, 20,20,10,False,None,None,True,10,)
+        elif answer == "b":
+            print("you walk for a whole day and pass out")
+            ending(1)
